ed8d525aab880389c8efbce45bc544837f0d3f44
David Latil <davidlatil@me.com>
2021-03-19 11:22:27 +0100

    Added physics and rover

---
+++ app/src/main/java/utils/Physics.java
@@ -0,0 +1,20 @@
+package utils;
+
+import models.Position;
+import models.Speed;
+
+public final class Physics {
+    public static Position updatePosition(Position p, Speed s, int ax, int ay, float t) {
+        int x1 = (int) (p.getX() + s.getX() * t + 1/2 * ax * Math.sqrt(t));
+        int y1 = (int) (p.getY() + s.getY() * t + 1/2 * ay * Math.sqrt(t));
+
+        return new Position(x1, y1);
+    }
+
+    public static Speed updateSpeed(Speed s, int ax, int ay, float t) {
+        int Vx = (int) (s.getX() + ax * t);
+        int Vy = (int) (s.getY() + ay * t);
+
+        return new Speed(Vx, Vy);
+    }
+}
