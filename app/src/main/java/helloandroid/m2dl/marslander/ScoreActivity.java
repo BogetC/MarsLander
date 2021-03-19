f9e987417a0dae8e292748b7755612c47451573f
BogetC <bogetcorentin@gmail.com>
2021-03-19 13:23:23 +0100

    feat: Handle score

--- app/src/main/java/helloandroid/m2dl/marslander/ScoreActivity.java
+++ app/src/main/java/helloandroid/m2dl/marslander/ScoreActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -33,6 +34,10 @@ public class ScoreActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+        Button playAgainButton = findViewById(R.id.playAgainButton);
+        playAgainButton.setOnClickListener(v -> {
+            // Lauch game again
+        });
     }
 
     @Override
